a1991f1d5b7841091154f87a9a489237a3eba3f4
markopmilo <marko.p.milovanovich@gmail.com>
2025-03-07 21:27:11 +0100

    Version 2 done

--- parser.py
+++ parser.py
@@ -1,116 +1,131 @@
-from sly import Lexer, Parser
-import pandas as pd
+from openpyxl import load_workbook
+from openpyxl.writer.excel import save_workbook
+from pybtex.database.output.bibtex import Writer
+from pybtex.database.input import bibtex
+from transliterate import translit
+import re
+from copy import copy
+from langdetect import detect, DetectorFactory
+DetectorFactory.seed = 0
+
+# Method for styling hyperlink cells when adding them
+def hyperlinkStyling():
+    # set the last cell to be a hyperlink
+    last_row = sheet.max_row
+    cell = sheet.cell(row=last_row, column=5)
+    left_cell = sheet.cell(row=last_row, column=4)
+
+    # styling for hyperlink
+    new_font = copy(left_cell.font)
+    new_font.underline = "single"
+    new_font.color = "0000FF"  # blue color in hex
+    cell.font = new_font
+    cell.hyperlink = link
 
 
-class BibTexLexer(Lexer):
-    tokens = {'ENTRY_KEY', 'KEY', 'VALUE', 'EQUALS'}
-    ignore = ' \t'
+bibtex_file = 'bells-2024-16.bib'
+parser = bibtex.Parser()
+bib_data = parser.parse_file(bibtex_file)
 
-    # Tokens
-    ENTRY_KEY = r'@[a-zA-Z]+\{[^,]+'  # Capture @article{2024:
-    KEY = r'[a-zA-Z_][a-zA-Z0-9_]*'  # Capture keys (author, orcid, journal, etc)
-    VALUE = r'\{([^}]*)\}'  # The values for the keys (Душанић, 0000-0002..., књижевна историја, etc)
-    EQUALS = r'='
+bibtex_dict = {}
+for entry_key, entry in bib_data.entries.items():
+    orcid_pattern = r"\b(\d{4}-\d{4}-\d{4}-\d{3}[\dX])\b" # find the individual orcids in the entry
+    orcids = re.findall(orcid_pattern, entry.fields['orcid'])
+    for i, author in enumerate(entry.persons['author']):
+        first = author.first_names[0] if author.first_names else ''
+        last = author.last_names[0] if author.last_names else ''
+        if len(orcids) > i :
+            bibtex_dict[(first, last)] = orcids[i]
+        else:
+            bibtex_dict[(first, last)] = None
 
-    ignore_newline = r'\n+'
-    ignore_comma = r',+'
-    ignore_closing_brace = r'\}'
+# Load the source Excel file
+source_file = "ORCID-FIL.xlsx"
+wb = load_workbook(source_file)
+sheet = wb.active
 
-    def error(self, t):
-        print("Illegal character '%s'" % t.value[0])
-        self.index += 1
+headers = [cell.value for cell in sheet[1]]  # Header row
+headers = [s.strip() if s is not None else None for s in headers] #strip header whitespace
 
+# indexes of the rows
+firstname_index = headers.index("Име")
+lastname_index = headers.index("Презиме")
+orcid_index = headers.index("ORCId идентификатор")
 
-class BibTexParser(Parser):
-    tokens = BibTexLexer.tokens
+excel_dict = {}
+i = 2
+# Iterate over each row
+for row in sheet.iter_rows(min_row=2, values_only=True):
+    first_cyr = row[firstname_index]  # First name in Cyrillic
+    last_cyr = row[lastname_index]  # Last name in Cyrillic
+    orcid = row[orcid_index]  # orcid
 
-    # Set up empty dict of keys
-    def __init__(self):
-        self.keys = {}
+    # turn cyrillic to latin
+    first_lat = translit(first_cyr, 'sr', reversed=True)
+    last_lat = translit(last_cyr, 'sr', reversed=True)
 
-    # capture @article line
-    @_('ENTRY_KEY')
-    def entry_key(self, p):
-        self.entry_key = p.ENTRY_KEY.split('{')[1]
+    # If the bibtex file had the orcid for this person, but the excel doesn't, add it
+    if orcid is None and bibtex_dict.keys().__contains__((first_lat, last_lat)) and bibtex_dict[(first_lat, last_lat)] is not None:
+        sheet.cell(row=i, column=orcid_index + 1).value = bibtex_dict[(first_lat, last_lat)]
+        link = ""
+        if orcid is not None:
+            link = f"https://orcid.org/{orcid}"
+        hyperlinkStyling()
+    excel_dict[(first_lat, last_lat)] = orcid
+    i += 1
 
-    # Analyze one line of bibliography
-    @_('KEY EQUALS VALUE')
-    def entry(self, p):
-        value = p.VALUE[1:-1] if p.VALUE != '{}' else ''  # remove curly brace
-        self.keys[p.KEY] = value
+print(excel_dict)
 
-    # recursively analyze lines as shown above
-    @_('entries entry')
-    @_('entry')
-    @_('entry_key')
-    def entries(self, p):
-        return self.keys
+common_names = set(bibtex_dict.keys()) & set(excel_dict.keys()) # List of names in both the bibtex and Excel
+modify_bibtex = {}
+for name in common_names:
+    print(bibtex_dict[name])
+    if bibtex_dict[name] is None and excel_dict[name] is not None: # There is data in the excel, but not bibtex
+        print(name)
+        print("is none")
+        modify_bibtex[name] = excel_dict[name]
 
-    # entry point
-    @_('entries')
-    def start(self, p):
-        return self.keys
+print(modify_bibtex)
 
-
-def load_excel_data(file_name, fields):
-    df = pd.read_excel(file_name)  # reads data from file
-    nameToId = dict(zip(df['author'], df['orcid']))
-    idToName = dict(zip(df['orcid'], df['author'])) # assuming one-to-one relationship, I presume this would change, fine for now
-
-    if not fields.get("orcid"):
-        fields['orcid'] = nameToId.get(fields.get("author")) or ''
-    if not fields.get("author"):
-        fields['author'] = idToName.get(fields.get("orcid")) or ''
-
-
-def format_bibtex(entry_key, fields):
-    # Using the new fields dictionary, with the excel data, make a new string for the entry
-    bibtex_string = f"@article{{{entry_key}, \n"
-    for key, value in fields.items():
-        bibtex_string += f"    {key} = {{{value}}},\n"
-    bibtex_string += "}"
-    return bibtex_string
-
-
-def parse_bibtex(bibtex_entry, excel_file_name):
-    # does the parsing work and returns the relevant data to the main method
-    lexer = BibTexLexer()
-    parser = BibTexParser()
-    tokens = lexer.tokenize(bibtex_entry)
-    fields = parser.parse(tokens)
-
-    # add excel data if a field is missing
-    if fields['orcid'] == "" or fields['author'] == "":
-        load_excel_data(excel_file_name, fields)
-
-    return parser.entry_key, fields
+# add excel data to bibtex
+for entry_key, entry in bib_data.entries.items():
+    orcid_pattern = r"\b(\d{4}-\d{4}-\d{4}-\d{3}[\dX])\b" # find the individual orcids in the entry
+    orcids = re.findall(orcid_pattern, entry.fields['orcid'])
+    for i, author in enumerate(entry.persons['author']):
+        first = author.first_names[0] if author.first_names else ''
+        last = author.last_names[0] if author.last_names else ''
+        if modify_bibtex.__contains__((first, last)):
+            if len(orcids) > 0:
+                entry.fields['orcid'] = entry.fields['orcid'] + " and " + modify_bibtex[(first, last)]
+            else:
+                entry.fields['orcid'] = modify_bibtex[(first, last)]
+            print("modified " + first + " " + last)
 
 
-if __name__ == '__main__':
-    bibtex_entry = """@article{2024:,
-    author       = {Душанић, Дуња},
-    showauthor   = {Разговор водила и с енглеског превела Дуња Душанић (12. март 2023.)},
-    orcid        = {},
-    title        = {После посткритике: разговор с Ритом Фелски},
-    journal      = {Књижевна историја},
-    year         = {2024},
-    volume       = {56},
-    number       = {183},
-    pages        = {--},
-    issn         = {0350-6428},
-    howpublished = {print},
-    language     = {serbian},
-    note         = {7},
-    doi          = {10.18485/kis.2024.56.183.7},
-    doiurl       = {http://doi.fil.bg.ac.rs/volume.php?pt=journals&issue=kis-2024-56-183&i=7},
-    url          = {http://doi.fil.bg.ac.rs/pdf/journals/kis/2024-183/kis-2024-56-183-7.pdf}
-}
-    """
+# People in the bibtex, not in the Excel
+add_to_excel = set(bibtex_dict.keys()) - set(excel_dict.keys())
+for name in add_to_excel:
+    # Transliterating into Cyrillic causes issues, here is somewhat of a solution
+    if detect(name[0] + " " + name[1]) in ['hr', 'sl']:  # if this classifies names as Croatian or Slovenian, transcribe
+        first = translit(name[0], 'sr', reversed=False)
+        last = translit(name[1], 'sr', reversed=False)
+    else:
+        first = name[0]
+        last = name[1]
+        # print(name[0] + " " + name[1] + " or " + first_cyr + " " + last_cyr + " is " + detect(name[0] + " " + name[1])) # testing
 
-    print("Previous BibTeX entry:")
-    print(bibtex_entry)
+    orcid = bibtex_dict[name]
+    link = ""
+    if orcid is not None:
+        link = f"https://orcid.org/{orcid}"
+    sheet.append(["", first, last, orcid, link])
+    hyperlinkStyling()
 
-    entry_info = parse_bibtex(bibtex_entry, "orcid.xlsx")
 
-    print("\nUpdated BibTeX entry:")
-    print(format_bibtex(entry_info[0], entry_info[1]))
+# Save excel changes a new file
+destination_file = "test.xlsx"
+wb.save(destination_file)
+
+writer = Writer()
+modified_bibtex_file = "test.bib"
+writer.write_file(bib_data, modified_bibtex_file)
