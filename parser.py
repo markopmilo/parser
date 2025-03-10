3a70b9629b00429c036b37215da55abf42fbc020
markopmilo <marko.p.milovanovich@gmail.com>
2025-03-10 01:15:10 +0100

    Finally all bugs fixed

--- parser.py
+++ parser.py
@@ -1,5 +1,4 @@
 from openpyxl import load_workbook
-from openpyxl.writer.excel import save_workbook
 from pybtex.database.output.bibtex import Writer
 from pybtex.database.input import bibtex
 from transliterate import translit
@@ -7,6 +6,11 @@ import re
 from copy import copy
 from langdetect import detect, DetectorFactory
 DetectorFactory.seed = 0
+
+# The default for this library is to use quotation marks instead of curly braces
+class CustomBibtexWriter(Writer):
+    def quote(self, s):
+        return '{' + s + '}'
 
 # Method for styling hyperlink cells when adding them
 def hyperlinkStyling():
@@ -22,8 +26,9 @@ def hyperlinkStyling():
     cell.font = new_font
     cell.hyperlink = link
 
-
 bibtex_file = 'bells-2024-16.bib'
+excel_file = "ORCID-FIL.xlsx"
+
 parser = bibtex.Parser()
 bib_data = parser.parse_file(bibtex_file)
 
@@ -32,17 +37,23 @@ for entry_key, entry in bib_data.entries.items():
     orcid_pattern = r"\b(\d{4}-\d{4}-\d{4}-\d{3}[\dX])\b" # find the individual orcids in the entry
     orcids = re.findall(orcid_pattern, entry.fields['orcid'])
     for i, author in enumerate(entry.persons['author']):
-        first = author.first_names[0] if author.first_names else ''
-        last = author.last_names[0] if author.last_names else ''
+        first = " ".join(author.first_names) if author.first_names else ''
+        last = " ".join(author.last_names) if author.last_names else ''
+        first = first.replace("Ð", "Đ").replace("ð", "đ") # Ð and Đ are of different unicode values...
+                                                                                   # Poludeo sam tražijući problem haha
+        last = last.replace("Ð", "Đ").replace("ð", "đ")
         if len(orcids) > i :
             bibtex_dict[(first, last)] = orcids[i]
         else:
             bibtex_dict[(first, last)] = None
 
-# Load the source Excel file
-source_file = "ORCID-FIL.xlsx"
-wb = load_workbook(source_file)
+wb = load_workbook(excel_file)
 sheet = wb.active
+
+# Remove empty rows from the sheet
+for row in range(sheet.max_row, 0, -1):
+    if all(cell.value in (None, "") for cell in sheet[row]):
+        sheet.delete_rows(row)
 
 headers = [cell.value for cell in sheet[1]]  # Header row
 headers = [s.strip() if s is not None else None for s in headers] #strip header whitespace
@@ -59,7 +70,8 @@ for row in sheet.iter_rows(min_row=2, values_only=True):
     first_cyr = row[firstname_index]  # First name in Cyrillic
     last_cyr = row[lastname_index]  # Last name in Cyrillic
     orcid = row[orcid_index]  # orcid
-
+    if first_cyr is None or last_cyr is None:
+        continue
     # turn cyrillic to latin
     first_lat = translit(first_cyr, 'sr', reversed=True)
     last_lat = translit(last_cyr, 'sr', reversed=True)
@@ -71,35 +83,28 @@ for row in sheet.iter_rows(min_row=2, values_only=True):
         if orcid is not None:
             link = f"https://orcid.org/{orcid}"
         hyperlinkStyling()
+        print("Modified " + first_lat + " " + last_lat + " from Bibtex to excel")
     excel_dict[(first_lat, last_lat)] = orcid
     i += 1
-
-print(excel_dict)
 
 common_names = set(bibtex_dict.keys()) & set(excel_dict.keys()) # List of names in both the bibtex and Excel
 modify_bibtex = {}
 for name in common_names:
-    print(bibtex_dict[name])
     if bibtex_dict[name] is None and excel_dict[name] is not None: # There is data in the excel, but not bibtex
-        print(name)
-        print("is none")
         modify_bibtex[name] = excel_dict[name]
-
-print(modify_bibtex)
 
 # add excel data to bibtex
 for entry_key, entry in bib_data.entries.items():
     orcid_pattern = r"\b(\d{4}-\d{4}-\d{4}-\d{3}[\dX])\b" # find the individual orcids in the entry
-    orcids = re.findall(orcid_pattern, entry.fields['orcid'])
     for i, author in enumerate(entry.persons['author']):
-        first = author.first_names[0] if author.first_names else ''
-        last = author.last_names[0] if author.last_names else ''
+        first = " ".join(author.first_names) if author.first_names else ''
+        last = " ".join(author.last_names) if author.last_names else ''
         if modify_bibtex.__contains__((first, last)):
-            if len(orcids) > 0:
+            if len(re.findall(orcid_pattern, entry.fields['orcid'])) > 0:
                 entry.fields['orcid'] = entry.fields['orcid'] + " and " + modify_bibtex[(first, last)]
             else:
                 entry.fields['orcid'] = modify_bibtex[(first, last)]
-            print("modified " + first + " " + last)
+            print("modified " + first + " " + last + " in the bibtex file")
 
 
 # People in the bibtex, not in the Excel
@@ -121,11 +126,11 @@ for name in add_to_excel:
     sheet.append(["", first, last, orcid, link])
     hyperlinkStyling()
 
-
-# Save excel changes a new file
+# Save excel changes a new file (can be changed to the same file to just edit rather than make a new one)
 destination_file = "test.xlsx"
 wb.save(destination_file)
 
-writer = Writer()
+# Save bibtex changes to a new file (can be changed to the same file to just edit rather than make a new one)
+writer = CustomBibtexWriter()
 modified_bibtex_file = "test.bib"
 writer.write_file(bib_data, modified_bibtex_file)
